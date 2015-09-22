16037a2c42c869db0039c54e2b30dc20e99684e9
changeandy <changeandy@gmx.net>
2015-09-21 22:43:51 -0700

    testcalls done
    This should be a decent testing ground. I made 2 files so they would be
    easier to debug. The problems I found are in the comments, but they may
    well be rooted in my ignorance more than anything else

--- testcall.s
+++ testcall.s
@@ -13,14 +13,7 @@ andi $t9, $t4, 15	# bitwise AND $t4 and 15 (0xFFFF) put in t9 (0xA)
 
 or $t0, $t3, $t4	# bitwise OR $t3 and $t4 put in t0 (0x2E)
 ori $t1, $t3, 17	# bitwise OR $t3 and $t4 put in t1 (0x37)
-lui $t2, 22		# take 22 (0x16), shift left by 16 bits and store in $t2 (0x160000)
-slt $t3, $t0, $t1	# check if $t0 is less than $t1, store result in $t3 (0x1)
-#beq Rsrc1, Rsrc2, raddr
-#bne Rsrc1, Rsrc2, raddr
-#j address
-#jal address
-#jr Rsrc
-#lw Rdest, offset (Radd)
-#sw Rsrc, offset (Radd)
+lui $t2, 22		# take 22 (0x16), shift left by 16 bits and store in $t2 (0x160000)  !!!!!!Problem - returns zero
+
 
 addi	$0,$0,0 #unsupported instruction, terminate
