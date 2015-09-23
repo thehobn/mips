a4b13931face317d1083e76638793390e43b0661
changeandy <changeandy@gmx.net>
2015-09-22 18:32:35 -0700

    Testfiles WIP
    Did some underrun and overrun testcases for ls and sw

---
+++ testLWunderrun.s
@@ -0,0 +1,4 @@
+addiu $t1, $0, 0x1000 	# put a base memory location in $t1 (first possible word)
+lui $t1, 0x0040
+lw $s1, -4($t1)		# copy a word from invalid location to $s1
+
---
+++ testSWoverrun.s
@@ -0,0 +1,3 @@
+addiu $t0, $0, 0x3ffc 	# put a base memory location in $t0 (last possible word)
+lui $t0, 0x0040
+sw $s1, 4($t0)		# copy a word from invalid location to $s1
