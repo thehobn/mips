e378335f9e19cc0748de8eadb6fc9fb1d2011a36
changeandy <changeandy@gmx.net>
2015-09-22 23:43:03 -0700

    extended Testaddiu with slt

--- s/testaddiu.s
+++ s/testaddiu.s
@@ -4,5 +4,7 @@ addiu $t0, $t0, 12	# add 12 to $t0 (0x4)
 addiu $t0, $t0, 12	# add 12 to $t0 (0x10)
 addiu $t1, $0, 0x800	# create the lowest negative number
 sll   $t1, $t1, 20	
+slt   $t2, $t1, $0 	# this should be true
 addiu $t1, $t1, -1	# create the highest positive number
+slt   $t2, $t1, $0 	# this should be false
 addi	$0,$0,0 #unsupported instruction, terminate
